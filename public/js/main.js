ffa893165cf6a13facb1bf8f2f19be076c12e1b4
Dean Marano <dmarano@groupon.com>
2013-08-16 00:01:33 -0500

    Add validations.

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,27 @@ $(function() {
   $('form').on('submit', function(e) {
     e.preventDefault();
     e.stopPropagation();
+
+    var name = $('input[name="name"]');
+    var email = $('input[name="email"]');
+    var telephone = $('input[name="telephone"]');
+    var inputs= [name, email, telephone];
+    hasError = false;
+
+    for(var i = 0; i < 3; i++) {
+      if(inputs[i].val() === "") {
+        hasError = true;
+        inputs[i].addClass('error');
+      }
+    }
+    if(hasError) {
+      $('.error-message').removeClass('hide');
+      return;
+    }
+
+    $('.error-message').addClass('hide');
+    $('.error').removeClass('error');
+
     successFn = function() {
       $('.callout').hide();
       $('.title-text').html("Congrats, help is on the way.<br />We'll contact you shortly.");
@@ -15,6 +36,6 @@ $(function() {
       data: $('form').serialize(),
       success: successFn
     });
-    $('input[type="submit"]').replaceWith('<img class="spinner" src="/ajaxSpinner.gif" />')
+    $('input[type="submit"]').replaceWith('<img class="spinner" src="/ajaxSpinner.gif" />');
   });
 });
