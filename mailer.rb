1f55dce4c99a47bae5039d01f655d990c2c5a9d8
Dean Marano <dmarano@groupon.com>
2013-08-16 00:53:08 -0500

    Change from address, refactor mailer.

--- mailer.rb
+++ mailer.rb
@@ -1,9 +1,9 @@
-require "net/smtp"
-require "time" # for rfc2822
+require 'net/smtp'
+require 'time'
 require 'mustache'
 
-FROM_ADDRESS = 'dean.marano@gmail.com'
-FROM_ADDRESS_PASSWORD = 'der4321r'
+FROM_ADDRESS = 'leads@bluecollarcredit.com'
+FROM_ADDRESS_PASSWORD = 'leadspassword'
 NOTIFICATION_ADDRESS = 'leads@bluecollarcredit.com'
 SERVER_ADDRESS = 'smtp.gmail.com'
 SERVER_PORT = 587
@@ -14,11 +14,7 @@ class Mailer
                                    :from_address => FROM_ADDRESS,
                                    :to_address => signup.email,
                                    :to_name => signup.name)
-    smtp = Net::SMTP.new(SERVER_ADDRESS, SERVER_PORT)
-    smtp.enable_starttls
-    smtp.start(SERVER_ADDRESS,FROM_ADDRESS,FROM_ADDRESS_PASSWORD,:login) do
-      smtp.send_message(mail_content, FROM_ADDRESS, signup.email)
-    end
+    send_email(mail_content, signup.email)
   end
 
   def self.send_notification(signup)
@@ -28,10 +24,14 @@ class Mailer
                                    :name => signup.name,
                                    :email => signup.email,
                                    :telephone => signup.telephone)
+    send_email(mail_content, NOTIFICATION_ADDRESS)
+  end
+
+  def self.send_email(mail_content, to_address)
     smtp = Net::SMTP.new(SERVER_ADDRESS, SERVER_PORT)
     smtp.enable_starttls
     smtp.start(SERVER_ADDRESS,FROM_ADDRESS,FROM_ADDRESS_PASSWORD,:login) do
-      smtp.send_message(mail_content, FROM_ADDRESS_PASSWORD, NOTIFICATION_ADDRESS)
+      smtp.send_message(mail_content, FROM_ADDRESS, to_address)
     end
   end
 end
